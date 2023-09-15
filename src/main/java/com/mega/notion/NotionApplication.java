0f992fd0239a958ab7e7ee3f26b6b1f811f60cb0
jsheo <tjrwjdgj@gamil.com>
2023-09-15 12:26:43 +0900

    fix: springboot version 변경 및 log4j2.xml 추가

--- src/main/java/com/mega/notion/NotionApplication.java
+++ src/main/java/com/mega/notion/NotionApplication.java
@@ -1,12 +1,15 @@
 package com.mega.notion;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class NotionApplication {
 
 	public static void main(String[] args) {
+		log.info("============================================ START !!!! ============================================");
 		SpringApplication.run(NotionApplication.class, args);
 	}
 
